6055ebf1c12d99bccbdf87dcffcd6ba2955d6244
hala <ha2051@hw.ac.uk>
2023-11-03 10:08:29 +0400

    week4 added

---
+++ week4/main.js
@@ -0,0 +1,20 @@
+var myCity = [
+
+{
+    "name" : "Dubai",
+    "country" : "UAE",
+    "place" : "Dubai Mall"
+} ,
+{
+    "name" : "Milan",
+    "country" : "Italy",
+    "place" : "Domo di milan"
+} ,
+{
+    "name" : "Abu Dhabi",
+    "country" : "UAE",
+    "place" : "Marina Mall"
+}
+]
+console.log(myCity[1].country);
+
