0cf28508d247418263bd792fa648caa9ee306fc6
hala <ha2051@hw.ac.uk>
2023-11-09 22:23:42 +0400

    fixing app.js

--- week4/weather/app.js
+++ week4/weather/app.js
@@ -1,34 +1,43 @@
-var cityInput = document.getElementById('cityInput');
-var searchButton = document.getElementById('btn');
-var weatherInfoDiv = document.getElementById('weather-info');
+const apiKey = '2e0bb3b26408a54823e7ddf16932990f'; // Wrap the API key in quotes
+const inputField = document.getElementById('cityInput');
+const searchButton = document.getElementById('searchButton');
+const weatherInfoDiv = document.getElementById('weatherInfo');
 
-searchButton.addEventListener('click', function () {
-  var city = cityInput.value.trim();
-  console.log(city);
-  if (city === '') {
-    alert('Error: City name cannot be empty!');
-  } else {
-    var ourRequest = new XMLHttpRequest();
+searchButton.addEventListener('click', () => {
+  const cityName = inputField.value.trim();
 
-    ourRequest.open('GET', `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2e0bb3b26408a54823e7ddf16932990f`);
-    ourRequest.onload = function () {
-      if (ourRequest.status >= 200 && ourRequest.status < 400) {
-        var data = JSON.parse(ourRequest.responseText);
-        const weatherInfo = "<p>The weather in " + data.name + " is " + data.weather[0].description + ".<br>The temperature is " + data.main.temp + "with a wind speed of " + data.wind.speed + "m/s.</p>";
-
-        weatherInfoDiv.insertAdjacentHTML('beforeend',weatherInfo);
-      } else {
-        var httpStatusError = "<p>HTTP Status Code Error</p>";
-        weatherInfoDiv.insertAdjacentHTML('beforeend',httpStatusError);
-      }
-    };
-
-    ourRequest.onerror = function () {
-      var networkError = "<p>Network Error</p>";
-      weatherInfoDiv.insertAdjacentHTML('beforeend',networkError);
-
-    };
-
-    ourRequest.send();
+  if (cityName === '') {
+    alert('Please enter a city name');
+    return;
   }
+
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      const weatherDescription = data.weather[0].description;
+      const mainTemperature = data.main.temp;
+      const windSpeed = data.wind.speed;
+
+      weatherInfoDiv.innerHTML = `
+        <p>Weather: ${weatherDescription}</p>
+        <p>Main Temperature: ${mainTemperature} K</p>
+        <p>Wind Speed: ${windSpeed} m/s</p>
+      `;
+    })
+    .catch(error => {
+      if (error.name === 'AbortError') {
+        alert('Request aborted');
+      } else if (error.name === 'TypeError') {
+        alert('Network error. Please check your internet connection');
+      } else {
+        alert('An error occurred. Please try again later');
+      }
+    });
 });
